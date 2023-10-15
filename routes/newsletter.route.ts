a6d81b989addfebd5dbcdbd507ea0d373cf36ea9
Dev-scott <sadoscott25@gmail.com>
2023-10-15 18:50:14 +0100

    newsletter route

--- routes/newsletter.route.ts
+++ routes/newsletter.route.ts
@@ -1,12 +1,45 @@
 import express from "express";
 import { authorizeRoles, isAutheticated } from "../middleware/auth";
+import {
+  createAudience,
+  deleteOneEmail,
+  getAllEmails,
+  getOneEmail,
+  saveEmail
+} from "../controllers/newsletter.controller";
 const newsletterRoute = express.Router();
 
-newsletterRoute.get(
-  "/get-all-notifications",
+newsletterRoute.post(
+  "/newletter/create-list",
   isAutheticated,
   authorizeRoles("admin"),
+  createAudience
 );
-newsletterRoute.put("/update-notification/:id", isAutheticated, authorizeRoles("admin"), );
+newsletterRoute.get(
+  "/newletter/save",
+  isAutheticated,
+  authorizeRoles("admin"),
+  saveEmail
+);
+newsletterRoute.get(
+  "/newletter/all-email",
+  isAutheticated,
+  authorizeRoles("admin"),
+  getAllEmails
+);
+
+newsletterRoute.get(
+  "/newsletter/:id",
+  isAutheticated,
+  authorizeRoles("admin"),
+  getOneEmail
+);
+
+newsletterRoute.get(
+  "/newsletter/:id",
+  isAutheticated,
+  authorizeRoles("admin"),
+  deleteOneEmail
+);
 
 export default newsletterRoute;
