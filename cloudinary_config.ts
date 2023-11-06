e3dd5984e72309ba00d14f2c50a7972186736ce7
Dev-scott <sadoscott25@gmail.com>
2023-11-06 23:06:47 +0100

    add watermark

--- cloudinary_config.ts
+++ cloudinary_config.ts
@@ -1,35 +1,27 @@
 require("dotenv").config();
 import cloudinary from "cloudinary";
 
-
-
 cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_SECRET_KEY
 });
 
-
-exports.uploads = (file:any , folder:any ,optionsOnUpload ={}) => {
-
-  return new Promise ((resolve) =>{
-
+exports.uploads = (file: any, folder: any, optionsOnUpload = {}) => {
+  return new Promise((resolve) => {
     cloudinary.v2.uploader.upload(
       file,
       {
-        resource_type:"auto",
-        folder:folder,
+        resource_type: "auto",
+        folder: folder,
         ...optionsOnUpload
       },
-      (error , result) =>{
+      (error, result) => {
         resolve({
-          url:result?.url,
-          secure_url:result?.secure_url
-        })
+          url: result?.url,
+          secure_url: result?.secure_url
+        });
       }
-    )
-
-
-  })
-
-}
+    );
+  });
+};
