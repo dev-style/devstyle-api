257847cbf746ff44c7cec9b4f19511fc1a3e4df9
artemesian <angelodiepe10@yahoo.com>
2023-11-12 13:47:50 +0100

    to adapt to serverless runtime, I changed the order of connection. connectDB first, then server can listen

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import {v2 as cloudinary} from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import http from "http";
 import connectDB from "./utils/db";
 import { initSocketServer } from "./socketServer";
@@ -6,18 +6,18 @@ import { app } from "./app";
 require("dotenv").config();
 const server = http.createServer(app);
 
-
 // cloudinary config
 cloudinary.config({
- cloud_name: process.env.CLOUD_NAME,
- api_key: process.env.CLOUD_API_KEY,
- api_secret: process.env.CLOUD_SECRET_KEY,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_SECRET_KEY,
 });
 
 initSocketServer(server);
 
 // create server
-server.listen(process.env.PORT, () => {
+connectDB().then(() => {
+  server.listen(process.env.PORT, () => {
     console.log(`Server is connected with port ${process.env.PORT}`);
-    connectDB();
+  });
 });
