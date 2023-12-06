1dd8d98036137a1307045f014bcca7e8a2bf17b1
Dev-scott <sadoscott25@gmail.com>
2023-12-06 09:07:16 +0100

    refactor(server): work on order analytic

--- controllers/analytics.controller.ts
+++ controllers/analytics.controller.ts
@@ -3,6 +3,7 @@ import ErrorHandler from "../utils/ErrorHandler";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import userModel from "../models/user.model";
 import OrderModel from "../models/social.model";
+import { generateLast12MothsData } from "../utils/analytics.generator";
 
 // get users analytics --- only for admin
 export const getUsersAnalytics = CatchAsyncError(
@@ -28,6 +29,10 @@ export const getGoodiesAnalytics = CatchAsyncError(
 export const getOrderAnalytics = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
+      const orders = await generateLast12MothsData(OrderModel);
+      res.status(200).json({
+        message: orders
+      });
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 500));
     }
