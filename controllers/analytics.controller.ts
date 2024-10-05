f704cebf701dde5936e6c0a611c0ba3330777e73
Dev-scott <sadoscott25@gmail.com>
2024-10-05 07:23:44 -0400

    fix: resolve issue in analytic controller

--- controllers/analytics.controller.ts
+++ controllers/analytics.controller.ts
@@ -2,15 +2,17 @@ import { Request, Response, NextFunction } from "express";
 import ErrorHandler from "../utils/ErrorHandler";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import userModel from "../models/user.model";
-import OrderModel from "../models/social.model";
 import { generateLast12MothsData } from "../utils/analytics.generator";
 import GoodieModel from "../models/goodie.model";
+import OrderModel from "../models/order.model";
 
 // get users analytics --- only for admin
 export const getUsersAnalytics = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       const users = await generateLast12MothsData(userModel);
+      console.log(" users i get",users)
+
       res.status(200).json({
         success: true,
         users,
@@ -26,6 +28,7 @@ export const getGoodiesAnalytics = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       const goodies = await generateLast12MothsData(GoodieModel);
+      console.log(" goodie i get",goodies)
 
       res.status(200).json({
         message: goodies,
@@ -41,6 +44,7 @@ export const getOrderAnalytics = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       const orders = await generateLast12MothsData(OrderModel);
+      console.log("orders i get",orders)
       res.status(200).json({
         message: orders,
       });
